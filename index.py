1a3479969fa7e7a49da954fec575eb5e3bf26a6e
Pazzy <69752194+Pazzy076@users.noreply.github.com>
2021-02-06 14:48:45 +0200

    main

---
+++ index.py
@@ -0,0 +1,10 @@
+import discord
+from discord.ext import commands
+
+client = commands.Bot(command_prefix = '!')
+
+@client.command()
+async def ping(ctx):
+    await ctx.send('Pong!')
+
+client.run('your-token-goes-here')
